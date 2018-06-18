58c4d38bdc80d3e0a80992cba8fb4856cfcfda24
S-Ozturk <39081080+S-Ozturk@users.noreply.github.com>
2018-06-18 15:45:12 +0200

    Improving adding and selecting materials by adding drag and drop support

--- src/main/java/com/project/catering/config/WebSecurityConfig.java
+++ src/main/java/com/project/catering/config/WebSecurityConfig.java
@@ -1,9 +1,5 @@
 package com.project.catering.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -19,7 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -36,15 +32,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		    .logout()
 		        .permitAll();
     }
-    
-
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
     	UserDetails user =
              User.withDefaultPasswordEncoder()
-                .username("user")
+                .username("user")  // userservıce.getbyıd(3).getusername()
                 .password("password")
                 .roles("USER")
                 .build();
@@ -54,9 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                    .password("password")
                    .roles("ADMIN")
                    .build();
-
-
-        return new InMemoryUserDetailsManager(user,admin);
+        	return new InMemoryUserDetailsManager(user,admin);
     }
-    
 }
